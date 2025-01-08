6812b44d8581ea224e629451445844916beb353e
toni Filho <oficinadosdrones@gmail.com>
2025-01-08 11:34:32 +0000

    created others components

---
+++ src/app/user-table/observable-table/observable-table.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-observable-table',
+  imports: [],
+  templateUrl: './observable-table.component.html',
+  styleUrl: './observable-table.component.css'
+})
+export class ObservableTableComponent {
+
+}
---
+++ src/app/user-table/signal-table/signal-table.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-signal-table',
+  imports: [],
+  templateUrl: './signal-table.component.html',
+  styleUrl: './signal-table.component.css'
+})
+export class SignalTableComponent {
+
+}
