7ffd1de9b9dcea7353e7a30e88d42a80d0b6f45c
toni Filho <oficinadosdrones@gmail.com>
2025-01-09 11:36:34 +0000

    Observable table was done

--- src/app/user-table/observable-table/observable-table.component.ts
+++ src/app/user-table/observable-table/observable-table.component.ts
@@ -4,6 +4,7 @@ import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { BehaviorSubject } from 'rxjs';
 import { IUserInterface } from '../../types/user.interface';
+import { ISortingInterface } from '../../types/sorting.interface';
 
 @Component({
   selector: 'app-observable-table',
@@ -13,15 +14,59 @@ import { IUserInterface } from '../../types/user.interface';
 })
 export class ObservableTableComponent {
   user: BehaviorSubject<IUserInterface[] | null> = new BehaviorSubject<
-    IUserInterface[] | null
+  IUserInterface[] | null
   >(null);
   columns: Array<keyof IUserInterface> = ['id', 'name', 'age'];
-
+  sorting: ISortingInterface = {order: 'asc', column: 'id'};
+  
   constructor(private userService: UserService) {
     this.userService.getUsers().subscribe({
-      next: (res) => {this.user.next(res)},
+      next: (res) => {
+        this.user.next(res);
+      },
       error: (e) => {},
       complete: () => {},
     });
   }
+  
+  capitalizer(str: string): string {
+    return str.charAt(0).toUpperCase() + str.substring(1);
+  }
+  isArrowDown(column: string): boolean {
+    return this.sorting.column === column && this.sorting.order === 'desc';
+  }
+  isArrowUp(column: string): boolean {
+    return this.sorting.column === column && this.sorting.order === 'asc';    
+  }
+  sortArrowTable(column: string): void {
+  const nextSort = this.isArrowUp(column) ? 'desc' : 'asc';
+  this.sorting = {column, order: nextSort};
+  this.sortAllData(column);
+  }
+
+  sortAllData(collumn: string):void {
+    switch (collumn) {
+      case 'age':
+      //console.log("Age: ", this.user.value);
+        const sortAgeAsc: IUserInterface[] = [...this.user.value as IUserInterface[]].sort((asc, desc ) => asc.age - desc.age);
+        const sortAgeDesc: IUserInterface[] = [...this.user.value as IUserInterface[]].sort((asc, desc ) => desc.age - asc.age);
+        this.isArrowUp(collumn) ? this.user.next(sortAgeAsc) : this.user.next(sortAgeDesc);
+        
+        break;
+      case 'name':
+        const sortNameAsc: IUserInterface[] = [...this.user.value as IUserInterface[]].sort((asc, desc) => asc.name.localeCompare(desc.name));
+        const sortNameDesc: IUserInterface[] = [...this.user.value as IUserInterface[]].sort((asc, desc) => desc.name.localeCompare(asc.name));
+        this.isArrowUp(collumn) ? this.user.next(sortNameAsc) : this.user.next(sortNameDesc);
+        
+        break;
+    
+      default:
+        const sortIdAsc: IUserInterface[] = [...this.user.value as IUserInterface[]].sort((asc, desc) => parseInt(asc.id) -parseInt(desc.id) );
+        const sortIdDesc: IUserInterface[] = [...this.user.value as IUserInterface[]].sort((asc, desc) => parseInt(desc.id) - parseInt(asc.id));
+        this.isArrowUp(collumn) ? this.user.next(sortIdAsc) : this.user.next(sortIdDesc);
+        break;
+    }
+  }
+
+
 }
