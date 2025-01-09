efc232d2c790b1a65bd6c5e51a50a7a29e22b1ce
toni Filho <oficinadosdrones@gmail.com>
2025-01-09 09:44:55 +0000

    create observable var and table on html

--- src/app/user-table/observable-table/observable-table.component.ts
+++ src/app/user-table/observable-table/observable-table.component.ts
@@ -1,13 +1,27 @@
+import { UserService } from './../user.service';
+
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-
+import { BehaviorSubject } from 'rxjs';
+import { IUserInterface } from '../../types/user.interface';
 
 @Component({
   selector: 'app-observable-table',
   imports: [CommonModule],
   templateUrl: './observable-table.component.html',
-  styleUrl: './../user-table/user-table.component.css'
+  styleUrl: './../user-table/user-table.component.css',
 })
 export class ObservableTableComponent {
+  user: BehaviorSubject<IUserInterface[] | null> = new BehaviorSubject<
+    IUserInterface[] | null
+  >(null);
+  columns: Array<keyof IUserInterface> = ['id', 'name', 'age'];
 
+  constructor(private userService: UserService) {
+    this.userService.getUsers().subscribe({
+      next: (res) => {this.user.next(res)},
+      error: (e) => {},
+      complete: () => {},
+    });
+  }
 }
